4e08d9e8cb88f95f4a18c7a354045042bd53312b
Hyodo <90432430+Naka-nishi-s@users.noreply.github.com>
2023-07-25 16:54:08 +0900

    WIP: complete prot

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # taiwa-app
+
 対話型のひな形作成アプリ
+
+## 使用方法
+
+1. パッケージをインストールします:
+
+   ```shell
+   npm install taiwa-app
+   ```
+
+2. アプリケーションを実行します:
+
+   ```shell
+   npx taiwa-app
+   ```
+
+3. 2 の際に出てくる質問で、yes を選ぶと以下が実行されます。
+
+   - `npm install hello-world-npm`
+   - index.html の作成
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 import { exec } from "child_process";
+import fs from "fs";
 import inquirer from "inquirer";
+import path from "path";
+import { fileURLToPath } from "url";
 
 inquirer
   .prompt([
@@ -14,6 +17,7 @@ inquirer
   ])
   .then((answer) => {
     if (answer.installType) {
+      // 追加でひな形作成（index.html的なものを追加してみる）
       exec("npm install hello-world-npm", (error, stdout, stderr) => {
         if (error) {
           console.log("エラーです。");
@@ -24,6 +28,21 @@ inquirer
           return;
         }
         console.log(`stdout:${stdout}`);
+
+        // 現在のファイルを取得
+        const __filename = fileURLToPath(import.meta.url);
+        // 現在のファイルのディレクトリを取得
+        const __dirname = path.dirname(__filename);
+        // index.htmlまでのフルパスを取得（現在のディレクトリ配下のindex.htmlの想定）
+        const srcPath = path.join(__dirname, "index.html");
+        // 対象のフルパスを取得（process.cwd()で対象PCの実行場所を取得し、その配下のindex.htmlを指定）
+        const destPath = path.join(process.cwd(), "index.html");
+
+        // copyFileで、現在のindex.htmを、対象の場所にコピー。（index.htmlがない場合は作成される）
+        fs.copyFile(srcPath, destPath, (err) => {
+          if (err) throw err;
+          console.log("index.html is created!");
+        });
       });
     }
   });
